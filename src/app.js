e67fdaf4e951a2fab936684c3282eab7d15e0bbf
Sunil <sunilloya.s@gmail.com>
2020-02-18 01:38:29 +0530

    changed appjs

--- src/app.js
+++ src/app.js
@@ -1,111 +1,101 @@
-const express = require('express');
 const path = require('path')
+const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const forecastutil = require('./utils/forecast')
-const geocastutil = require('./utils/geocode')
-const app = express();
+const app = express()
 
-const publicDirPath = path.join(__dirname, "../public");
-const viewspath = path.join(__dirname, "../templates/views");
-const partialPath = path.join(__dirname, "../templates/partials")
-// console.log(publicDirPath);
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-app.set('view engine', 'hbs');
-app.set('views', viewspath)
-app.use(express.static(publicDirPath));
-hbs.registerPartials(partialPath)
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: "weather app",
-        name: "Sunil"
+        title: 'Weather',
+        name: 'Sunil Loya'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: "About",
-        name: 'Sunil'
+        title: 'About Me',
+        name: 'Sunil Loya'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: "Help",
-        name: 'Sunil'
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Sunil Loya'
     })
 })
 
-/* app.get('', (req, res) => {
-    res.send("<h1>Home Page! Welcome express</h1>");
-});
-
-app.get('/help', (req, res) => {
-    res.send({
-        name: "Sunil",
-        Location: "Pune"
-    });
-})
-app.get('/about', (req, res) => {
-    res.send('<h3>about express!!</h3>')
-}); */
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
         return res.send({
-            error: 'PLese provide address'
+            error: 'You must provide an address!'
         })
     }
-    console.log(req)
-    let forecstdt;
-    geocastutil(req.query.address, (error, { latitude, longitude, location }) => {
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
         if (error) {
-            return res.send({
-                error: error
-            })
+            return res.send({ error })
         }
 
-        forecastutil(latitude, longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
-                return res.send({
-                    error: error
-                })
+                return res.send({ error })
             }
-            // console.log("forecastData:::" + forecastData)
+
             res.send({
-                address: req.query.address,
                 forecast: forecastData,
-                location: "San Fransis"
-            });
+                location,
+                address: req.query.address
+            })
         })
     })
+})
 
-});
 app.get('/products', (req, res) => {
     if (!req.query.search) {
         return res.send({
-            error: 'No products found'
+            error: 'You must provide a search term'
         })
     }
+
+    console.log(req.query.search)
     res.send({
         products: []
     })
+})
 
-})
 app.get('/help/*', (req, res) => {
-    res.render("404", {
-        title: '404 help',
-        name: 'Sunil',
-        errorMsg: "Help article not found"
-    })
-})
-app.get('*', (req, res) => {
-    res.render("404", {
+    res.render('404', {
         title: '404',
-        name: 'Sunil',
-        errorMsg: "Page not found"
+        name: 'Sunil Loya',
+        errorMessage: 'Help article not found.'
     })
 })
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Sunil Loya',
+        errorMessage: 'Page not found.'
+    })
+})
+
 app.listen(3000, () => {
-    console.log("Server is up and running");
+    console.log('Server is up on port 3000.')
 })
